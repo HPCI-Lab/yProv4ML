be3a5f36364a33ff4be073f51940775841707ebe
Mona Lisa <gabriele.padovani@gmail.com>
2026-02-17 10:34:48 -0500

    adjusted prov getters

--- yprov4ml/utils/prov_getters.py
+++ yprov4ml/utils/prov_getters.py
@@ -1,83 +1,50 @@
 
-# KEEP THESE FOR NAN eval() #
-import numpy as np
-from torch import nan
-#############################
-
 import pandas as pd
-from yprov4ml.utils.time_utils import timestamp_to_seconds
-from typing import Optional
+import json
 
-def get_metrics(data, keyword=None):
-    ms = data["entity"].keys()
-    if keyword is None:
-        return ms
-    else:
-        return [m for m in ms if keyword in m]
+def get_activities(json_file_or_path): 
+    if isinstance(json_file_or_path, str): 
+        json_file_or_path = json.load(open(json_file_or_path))
+    return list(json_file_or_path["activity"].keys())
 
-def get_metric(data, metric, time_in_sec=False, time_incremental=False, sort_by=None, start_at=None, end_at=None):
+def get_parameters(json_file_or_path): 
+    if isinstance(json_file_or_path, str): 
+        json_file_or_path = json.load(open(json_file_or_path))
 
-    if metric not in data["entity"].keys(): 
-        raise AttributeError(f">get_metric({metric}) not found in prov file")
+    parameters = []
+    for ent_name, ent in json_file_or_path["entity"].items(): 
+        ks = ent.keys()
+        if "prov:label" in ks and "prov:value" in ks: 
+            parameters.append(ent_name)
+    return parameters
 
-    epochs = eval(data["entity"][metric]["prov-ml:metric_epoch_list"])
-    values = eval(data["entity"][metric]["prov-ml:metric_value_list"])
-    times = eval(data["entity"][metric]["prov-ml:metric_timestamp_list"])
+def get_parameter(json_file_or_path, parameter_name): 
+    if isinstance(json_file_or_path, str): 
+        json_file_or_path = json.load(open(json_file_or_path))
 
-    start_at = 0 if start_at is None else start_at
-    end_at = len(epochs) if end_at is None else end_at        
+    return json_file_or_path["entity"][parameter_name]
 
-    epochs = epochs[start_at:end_at]
-    values = values[start_at:end_at]
-    times = times[start_at:end_at]
+def get_metrics(json_file_or_path): 
+    if isinstance(json_file_or_path, str): 
+        json_file_or_path = json.load(open(json_file_or_path))
     
-    # convert to minutes and sort
-    if time_in_sec:
-        times = [timestamp_to_seconds(ts) for ts in times]
-        
-    df = pd.DataFrame({"epoch": epochs, "value": values, "time": times})#.drop_duplicates()
-    if time_incremental: 
-        df["time"] = df["time"].diff().fillna(0)
+    metrics = []
+    for ent_name, ent in json_file_or_path["entity"].items(): 
+        if "prov:type" in ent.keys():
+            if ent["prov:type"] == "provml:Metric": 
+                metrics.append(ent_name)
 
-    if sort_by is not None: 
-        df = df.sort_values(by=sort_by)
-    
-    return df
+    return metrics
 
-def get_avg_metric(data, metric):
-    values = eval(data["entity"][metric]["prov-ml:metric_value_list"])
-    return sum(values) / len(values)
+def get_metric_metadata(json_file_or_path, metric_name): 
+    if isinstance(json_file_or_path, str): 
+        json_file_or_path = json.load(open(json_file_or_path))
 
-def get_sum_metric(data, metric):
-    values = eval(data["entity"][metric]["prov-ml:metric_value_list"])
-    return sum(values)
+    return json_file_or_path["entity"][metric_name]
 
-def get_metric_time(data, metric, time_in_sec=False): 
-    times = eval(data["entity"][metric]["prov-ml:metric_timestamp_list"])
-    if time_in_sec:
-        times = [timestamp_to_seconds(ts) for ts in times]
-    return max(times) - min(times)
+def get_metric_data(json_file_or_path, metric_name): 
+    if isinstance(json_file_or_path, str): 
+        json_file_or_path = json.load(open(json_file_or_path))
 
-def get_params(data, param):
-    return [data["entity"][ent]["prov-ml:parameter_value"] for ent in data["entity"].keys() if param in ent]
-
-def get_param(data, param): 
-    if param in data["entity"].keys():
-        return data["entity"][param]["prov-ml:parameter_value"]
-    return None
-
-
-def get_experiemnt_id(data) -> Optional[str]:
-    return list(data["activity"].keys())[0]
-
-def get_execution_command(data) -> Optional[str]:
-    return get_param(data, "prov-ml:execution_command")
-
-def get_source_code(data) -> Optional[str]: 
-    return get_param(data, "prov-ml:source_code")
-
-def get_inputs(data): 
-    return get_params(data, "prov-ml:input")
-
-def get_outputs(data): 
-    return get_params(data, "prov-ml:output")
+    metric_path = json_file_or_path["entity"][metric_name]["dcterms:identifier"]
+    return pd.read_csv(metric_path)
