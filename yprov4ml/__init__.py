d4dfb9cc8abd1b021f3629bc2511b7e6d50c6bd5
Mona Lisa <gabriele.padovani@gmail.com>
2026-06-09 09:34:12 -0400

    removed codecarbon

--- yprov4ml/__init__.py
+++ yprov4ml/__init__.py
@@ -1,7 +1,7 @@
 
 from . import * 
 from .logging_aux import *
-from .prov4ml import *
+from .functs import start_run, end_run
 from .wrappers.tracked_function import ProvenanceTrackedFunction
 from .wrappers.tracked_model import ProvenanceTrackedModel
 from .wrappers.tracked_layers import WeightDistributionTrackedModel
